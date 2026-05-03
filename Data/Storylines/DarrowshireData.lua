02d7fdd3316959a16a61c0d448f03971c670a27d
Rafael Polutta <contact@hejrafa.com>
2026-05-03 15:57:35 +0200

    Update Darrowshire and Brazier covers

--- Data/Storylines/DarrowshireData.lua
+++ Data/Storylines/DarrowshireData.lua
@@ -15,7 +15,7 @@ SM.DarrowshireData = {
     color = { 0.68, 0.62, 0.48 },
     icon = 134331,
     portraitDisplayID = 7069,
-    adventureCoverTexture = 131871,
+    adventureCoverTexture = 131859,
     adventureCoverIsLoadingScreen = true,
 
     startQuest = { id = 5142, altIds = { 5601 }, name = "Little Pamela", npc = "Marlene Redpath", location = "Sorrow Hill, Western Plaguelands" },
